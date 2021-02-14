a93606e19ba38b7d04f89a0fb9fd1d3e0184cbd2
himsangseung <himsangseung@hotmail.com>
2021-02-14 00:26:12 -0500

    devel - depend file pushed

--- CarND-Capstone/ros/build/catkin_generated/setup_cached.sh
+++ CarND-Capstone/ros/build/catkin_generated/setup_cached.sh
@@ -7,9 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/himsangseung/SelfDrivingCarNanoDegree/CarND-Capstone/ros/devel:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH="/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH"
-export PATH='/opt/ros/kinetic/bin:/usr/local/cuda/bin:/usr/local/cuda-11.2/bin:/home/himsangseung/bin:/home/himsangseung/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/himsangseung/.fzf/bin'
 export PWD='/home/himsangseung/SelfDrivingCarNanoDegree/CarND-Capstone/ros/build'
-export ROSLISP_PACKAGE_DIRECTORIES='/home/himsangseung/SelfDrivingCarNanoDegree/CarND-Capstone/ros/devel/share/common-lisp'
-export ROS_PACKAGE_PATH="/home/himsangseung/SelfDrivingCarNanoDegree/CarND-Capstone/ros/src:$ROS_PACKAGE_PATH"
